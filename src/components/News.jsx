1510f33dbd4abb3b6ce3a00c6c6295cc2c35dc61
Аделия Тлеубаева <adeliyatleubayeva@Adeliyamacbook.local>
2025-01-15 23:30:01 +0500

    init

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,18 @@
-export default function News({ author, description, image }) {
+import { useState } from "react";
+
+export default function News({ author, description, image, time }) {
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+
+  const handleLike = () => {
+    if (liked) {
+      setLikesCount(likesCount - 1);
+    } else {
+      setLikesCount(likesCount + 1);
+    }
+    setLiked(!liked);
+  };
+
   return (
     <li>
       <p>
@@ -6,6 +20,13 @@ export default function News({ author, description, image }) {
       </p>
       <p>{description}</p>
       {image && <img src={image} alt="news" />}
+      <p className="time">{time}</p>
+      <div className="like-container">
+        <button onClick={handleLike} className="like-button">
+          {liked ? "💙" : "🤍"} {}
+        </button>
+        <span className="likes-count">{likesCount}</span> {}
+      </div>
     </li>
   );
 }
